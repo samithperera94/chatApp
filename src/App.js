cb3dcf81bdf96f5e2551207f85ca47c75a8373b5
samithperera94 <samiththilanka94@gmail.com>
2023-01-17 19:09:48 +0530

    firebase app : initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBTL7UZWPZNuR6AtZI8fyNBSNuqZYTlrw0",
+  authDomain: "react-chat-b96ff.firebaseapp.com",
+  projectId: "react-chat-b96ff",
+  storageBucket: "react-chat-b96ff.appspot.com",
+  messagingSenderId: "290290509540",
+  appId: "1:290290509540:web:0da07293413ad56dd6aef1"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
